ec8bd6b3712cd7cde8e569a705e3bd9bd766b57e
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-01-23 20:43:29 +0100

    Gate `captureOwnerStack` access on `experimental.reactOwnerStack`

--- packages/next/src/build/webpack/plugins/define-env-plugin.ts
+++ packages/next/src/build/webpack/plugins/define-env-plugin.ts
@@ -287,6 +287,8 @@ export function getDefineEnv({
       : undefined),
     'process.env.__NEXT_EXPERIMENTAL_NEW_DEV_OVERLAY':
       config.experimental.newDevOverlay ?? false,
+    'process.env.__NEXT_REACT_OWNER_STACK':
+      config.experimental.reactOwnerStack ?? false,
   }
 
   const userDefines = config.compiler?.define ?? {}
