e13f79763f28a3613d1b2661ac0dda10ca23397d
Tobias Koppers <tobias.koppers@googlemail.com>
2025-01-23 18:34:44 +0100

    build doesn't need dependency tracking

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -1385,6 +1385,7 @@ export default async function build(
           'last 1 Chrome versions, last 1 Firefox versions, last 1 Safari versions, last 1 Edge versions',
         ]
 
+        const persistentCaching = isPersistentCachingEnabled(config)
         const project = await bindings.turbo.createProject(
           {
             projectPath: dir,
@@ -1417,8 +1418,9 @@ export default async function build(
             browserslistQuery: supportedBrowsers.join(', '),
           },
           {
-            persistentCaching: isPersistentCachingEnabled(config),
+            persistentCaching,
             memoryLimit: config.experimental.turbo?.memoryLimit,
+            dependencyTracking: persistentCaching,
           }
         )
 
